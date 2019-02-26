9c97b54a31b11467b00cab2637de97e8bcaac021
toopukondabhaskar <bhaskar.t325@gmail.com>
2019-02-26 15:14:04 +0530

    codedeploy resion

--- vendor/bin/awscodedeploy.sh
+++ vendor/bin/awscodedeploy.sh
@@ -1,1 +1,1 @@
-aws deploy create-deployment --application-name circlecidemo --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name sample --description "My demo deployment" --s3-location bucket=s3awscirclecidemo,bundleType=zip,key=zipped_dir3.zip
+aws deploy create-deployment --application-name circlecidemo --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name sample --description "My demo deployment" --s3-location bucket=s3awscirclecidemo,bundleType=zip,key=zipped_dir3.zip --resgion us-east-1
