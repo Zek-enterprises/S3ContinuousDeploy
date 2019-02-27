11a078dbceb14610bf4c4d285e922c14e1d6bff8
toopukondabhaskar <bhaskar.t325@gmail.com>
2019-02-27 11:01:31 +0530

    env variable

--- vendor/bin/awscodedeploy.sh
+++ vendor/bin/awscodedeploy.sh
@@ -1,3 +1,1 @@
-AWS_CODE_DEPLOY_REGION: us-east-1
-
 aws deploy create-deployment --application-name circlecidemo --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name sample --description "My demo deployment" --s3-location bucket=s3awscirclecidemo,bundleType=zip,key=zipped_dir3.zip
