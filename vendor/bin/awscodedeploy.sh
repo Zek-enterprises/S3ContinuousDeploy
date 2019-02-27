5c3fb1b8b04169a3909e0ba046b6a97787b1eab3
toopukondabhaskar <bhaskar.t325@gmail.com>
2019-02-27 12:50:51 +0530

    aws cli error

--- vendor/bin/awscodedeploy.sh
+++ vendor/bin/awscodedeploy.sh
@@ -1,6 +1,7 @@
-aws configure
-Default region name : acd
-AWS Access Key ID : xxxxx
-AWS Secret Access Key : zzzzz
+echo $region
+
+aws configure set aws_access_key_id $aws_access_key_id
+aws configure set aws_secret_access_key $screct_key_id
+aws configure set default.region $region
 
 aws deploy create-deployment --application-name circlecidemo --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name sample --description "My demo deployment" --s3-location bucket=s3awscirclecidemo,bundleType=zip,key=zipped_dir3.zip
