b174949315b236bdefb926ee7f3dfc9f06544fd9
toopukondabhaskar <bhaskar.t325@gmail.com>
2019-02-27 12:30:08 +0530

    aws cli

--- vendor/bin/awscodedeploy.sh
+++ vendor/bin/awscodedeploy.sh
@@ -1,4 +1,5 @@
 aws configure
-Default region name : us-east-1
-
+Default region name : region
+AWS Access Key ID : aws_access_key_id
+AWS Secret Access Key : secret_key_id
 aws deploy create-deployment --application-name circlecidemo --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name sample --description "My demo deployment" --s3-location bucket=s3awscirclecidemo,bundleType=zip,key=zipped_dir3.zip
